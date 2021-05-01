ef972ba19286d1a9085fc1871d745f8a8a1d78e0
RudraUX <pollabrock@gmail.com>
2021-05-01 19:48:48 +0530

    database config done

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 //Middleware
-
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
